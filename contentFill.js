ddeb01a458241031e5652b3ff6dcb8fcb47a96d6
Marucik <rakoczy.mk@gmail.com>
2019-04-26 15:18:16 +0200

    Base for displaying content

---
+++ contentFill.js
@@ -0,0 +1,5 @@
+let content = data;
+
+for (let i = 0; i < content.projects.length; i++) {
+  const element = content.projects[i];
+}
---
+++ data.js
@@ -0,0 +1,99 @@
+var data = {
+    "projects": [
+        {
+            "name": "Ułatwiaczek",
+            "description": "Aplikacja ułatwiająca liczenie łatwości sprawdzianów",
+            "link": "https://github.com/Marucik/Ulatwiaczek",
+            "isDone": true
+        },
+        {
+            "name": "Absolwenci.PRO",
+            "description": "Baza danych absolwentów szkoły",
+            "link": "https://github.com/Dawiducik/Absolwenci.pro",
+            "isDone": true
+        },
+        {
+            "name": "Vue_XO",
+            "description": "Prosta gra w kółko i krzyżyk oparta na Vue i socketach",
+            "link": "https://marucik.github.io/vue_XO/",
+            "isDone": true
+        },
+        {
+            "name": "JS-calculator",
+            "description": "Kalkulator prosty",
+            "link": "https://marucik.github.io/JS-calculator/",
+            "isDone": true
+        },
+        {
+            "name": "JS-ASCII-Clock",
+            "description": "Zegarek w stylu ASCI-art",
+            "link": "https://marucik.github.io/JS-ASCII-Clock/",
+            "isDone": true
+        },
+        {
+            "name": "JS-ASCII-Clock",
+            "description": "Zegarek w stylu ASCI-art",
+            "link": "https://marucik.github.io/JS-ASCII-Clock/",
+            "isDone": true
+        }
+    ],
+    "tech": {
+        "planing": [
+            {
+                "name": "C#",
+                "description": "poziom wyższy niż podstawowy"
+            },
+            {
+                "name": "C++",
+                "description": "poziom wyższy niż podstawowy"
+            }
+        ],
+        "inProgress": [
+            {
+                "name": "React",
+                "description": ""
+            }
+        ],
+        "learnt": [
+            {
+                "name": "JavaScript",
+                "description": "Med"
+            },
+            {
+                "name": "HTML 5",
+                "description": "High"
+            },
+            {
+                "name": "CSS 3",
+                "description": "High"
+
+            },
+            {
+                "name": "PHP",
+                "description": "Med"
+            },
+            {
+                "name": "SQL",
+                "description": "Med"
+            },
+            {
+                "name": "C++",
+                "description": "Beg"
+            },
+            {
+                "name": "C#",
+                "description": "Beg"
+            },
+            {
+                "name": "Node.js",
+                "description": "Med"
+            }
+        ]
+    },
+"contact": [
+        {
+            "name": "Email",
+            "value": "Rakoczymarcin@outlook.com"
+        }
+    ]
+}
