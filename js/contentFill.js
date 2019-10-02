addfdf92bf10ec898ac139e0d8cf4b5bb2a389aa
Marucik <rakoczy.mk@gmail.com>
2019-10-02 22:13:52 +0200

    New design (#6)
    * Clearnig repo
    
    * Base of new design
    
    * Final base of new design
    
    * Fancy button hover
    
    * Base for displaying content
    
    * Done projects glider
    
    * Done almost all sections for desktop
    
    * v1 (Responsive)

---
+++ js/contentFill.js
@@ -0,0 +1,55 @@
+let content = data;
+let gliderElement = document.querySelector('.glider-track');
+let techContainerElement = document.querySelector('.technologiesContainer');
+
+for (let i = 0; i < content.projects.length; i++) {
+  let object = content.projects[i];
+
+  let card = document.createElement('div');
+  card.classList.add('carousellElement');
+
+  let title = document.createElement('h2');
+  title.classList.add('textCenter');
+  title.textContent = object.name;
+
+  let link = document.createElement('a');
+  let href = document.createAttribute('href');
+  let target = document.createAttribute('target');
+  href.value = object.link;
+  target.value = '_blank';
+  link.setAttributeNode(href);
+  link.setAttributeNode(target);
+
+  let icon = document.createElement('i');
+  icon.classList.add('fab', 'fa-github-alt', 'fa-2x');
+
+  let description = document.createElement('p');
+  description.classList.add('textCenter');
+  description.textContent = object.description;
+
+  link.appendChild(icon);
+  title.appendChild(link);
+
+  card.appendChild(title);
+  card.appendChild(description);
+
+  gliderElement.appendChild(card);
+}
+
+for (let i = 0; i < content.tech.length; i++) {
+  const object = content.tech[i];
+
+  let block = document.createElement('div');
+  block.classList.add('techBlock');
+
+  let name = document.createElement('p');
+  name.textContent = object.name;
+
+  let level = document.createElement('p');
+  level.textContent = object.level;
+
+  block.appendChild(name);
+  block.appendChild(level);
+
+  techContainerElement.appendChild(block);
+}
---
+++ js/data.js
@@ -0,0 +1,68 @@
+var data = {
+  projects: [
+    {
+      name: 'Ułatwiaczek',
+      description: 'Aplikacja ułatwiająca liczenie łatwości sprawdzianów',
+      link: 'https://github.com/Marucik/Ulatwiaczek'
+    },
+    {
+      name: 'Absolwenci.PRO',
+      description: 'Baza danych absolwentów szkoły',
+      link: 'https://github.com/Dawiducik/Absolwenci.pro'
+    },
+    {
+      name: 'Vue_XO',
+      description: 'Prosta gra w kółko i krzyżyk oparta na Vue i socketach',
+      link: 'https://marucik.github.io/vue_XO/'
+    },
+    {
+      name: 'JS-calculator',
+      description: 'Kalkulator prosty',
+      link: 'https://marucik.github.io/JS-calculator/'
+    },
+    {
+      name: 'JS-ASCII-Clock',
+      description: 'Zegarek w stylu ASCI-art',
+      link: 'https://marucik.github.io/JS-ASCII-Clock/'
+    },
+    {
+      name: 'JS-ASCII-Clock',
+      description: 'Zegarek w stylu ASCI-art',
+      link: 'https://marucik.github.io/JS-ASCII-Clock/'
+    }
+  ],
+  tech: [
+    {
+      name: 'JavaScript',
+      level: 'Medium'
+    },
+    {
+      name: 'HTML 5',
+      level: 'Medium'
+    },
+    {
+      name: 'CSS 3',
+      level: 'Medium'
+    },
+    {
+      name: 'PHP',
+      level: 'Begginer'
+    },
+    {
+      name: 'SQL',
+      level: 'Begginer'
+    },
+    {
+      name: 'C++',
+      level: 'Begginer'
+    },
+    {
+      name: 'C#',
+      level: 'Begginer'
+    },
+    {
+      name: 'Node.js',
+      level: 'Medium'
+    }
+  ]
+};
