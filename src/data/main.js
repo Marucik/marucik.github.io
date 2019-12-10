01177673b8ccdc890d54288904a2fd48ad8dbf74
Marucik <rakoczy.mk@gmail.com>
2019-12-10 15:06:11 +0100

    Updated content to english

--- src/data/main.js
+++ src/data/main.js
@@ -2,32 +2,32 @@ export default {
   projects: [
     {
       name: "Ułatwiaczek",
-      description: "Aplikacja ułatwiająca liczenie łatwości sprawdzianów",
+      description: "Application for calculating exam tasks difficulty",
       link: "https://github.com/Marucik/Ulatwiaczek"
     },
     {
       name: "Absolwenci.PRO",
-      description: "Baza danych absolwentów szkoły",
+      description: "School graduates database",
       link: "https://github.com/Dawiducik/Absolwenci.pro",
       live: "http://absolwenci.zstmielec.pl/"
     },
     {
       name: "Vue_XO",
-      description: "Prosta gra w kółko i krzyżyk oparta na Vue i socketach",
+      description: "Simple Tic Tac Toe game based on Vue and Sockets",
       link: "https://github.com/Marucik/vue_XO",
       live: "https://marucik.github.io/vue_XO/"
     },
     {
       name: "JS-calculator",
-      description: "Kalkulator prosty",
+      description: "Simple calculator",
       link: "https://github.com/Marucik/JS-calculator",
       live: "https://marucik.github.io/JS-calculator/"
     },
     {
       name: "JS-ASCII-Clock",
-      description: "Zegarek w stylu ASCI-art",
-      link: "https://marucik.github.io/JS-ASCII-Clock/",
-      live: "https://github.com/Marucik/JS-ASCII-Clock/"
+      description: "Clock in ASCII style",
+      link: "https://github.com/Marucik/JS-ASCII-Clock/",
+      live: "https://marucik.github.io/JS-ASCII-Clock/"
     }
   ],
   tech: [
